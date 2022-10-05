db81dfe19e1466d74735d85377b215712759f259
Ahsan Shaikh <ahsonshaikh616@gmail.com>
2022-10-05 21:47:49 +0500

    Initial Commit

---
+++ NodeJS+Nginx-Webserver/Amazon Linux Distribution/SettingUp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install --lts
+
+node -e "console.log('Running Node.js ' + process.version)"
+
+#Installing Nginx Server -- 
+
+#will enable and install the package from extras.
+sudo amazon-linux-extras install -y nginx1
+
+
+
